72c70c593e028fd62471b8d6fecafe233aefcad1
Nick <101208971+Dogober@users.noreply.github.com>
2022-10-19 00:05:33 +0300

    create-react-app && lines-logic

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,27 @@
+import { observer } from 'mobx-react-lite';
+import { FC, useEffect, useRef } from 'react';
+import canvasState from '../store/canvasState';
+import '../styles/canvas.scss'
+
+const Canvas: FC = observer(() => {
+
+    const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current)
+        canvasState.listen()
+    }, [])
+
+    return (
+        <div className='canvas'>
+            <canvas 
+                width={800} 
+                height={500}
+                ref={canvasRef}
+            />
+            <button className='collapse'>Collapse lines</button>
+        </div>
+    );
+});
+
+export default Canvas;
